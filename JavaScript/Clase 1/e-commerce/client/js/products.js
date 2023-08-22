bc1f8184f7fe09d503f740181b747f925f5eb9a5
Gustavo Ortiz <92409193+Gustaf02@users.noreply.github.com>
2023-08-22 12:02:48 -0300

    1º clase de JavaScript para la realización del e-commerce

---
+++ JavaScript/Clase 1/e-commerce/client/js/products.js
@@ -0,0 +1,24 @@
+const productos = [
+    {
+        id: 1,
+        productName: "Brochette de verduras",
+        price: 2800,
+        quanty: 1,
+        img: "/client/media/brochette_de_verduras.png",
+    },
+    {
+        id: 2,
+        productName: "Hamburguesa de soja",
+        price: 2500,
+        quanty: 1,
+        img: "/client/media/hamburguesa_de_soja.png",
+    },
+    {
+        id: 3,
+        productName: "Salteado de verduras",
+        price: 1500,
+        quanty: 1,
+        img: "/client/media/salteado_de_verduras.png",
+    },
+    
+];
