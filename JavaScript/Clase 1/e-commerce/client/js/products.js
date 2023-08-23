30cf8f4ca84868e8533d835a0b0f1a22e1e1cbc2
Leonardo Montiel <106494867+Leonardo-Montiel@users.noreply.github.com>
2023-08-22 23:22:01 -0300

    Agrego nuevos productos al menú

--- JavaScript/Clase 1/e-commerce/client/js/products.js
+++ JavaScript/Clase 1/e-commerce/client/js/products.js
@@ -21,4 +21,47 @@ const productos = [
         img: "/client/media/salteado_de_verduras.png",
     },
     
+
+    {
+        id: 4,
+        productName: "Proteico de vegetales",
+        price: 2800,
+        quanty: 1,
+        img: "/client/media/Proteico_de_vegetales.jpg",
+    },
+    {
+        id: 5,
+        productName: "Sushi con degustación de vinos (para compartir)",
+        price: 3200,
+        quanty: 1,
+        img: "/client/media/Sushi_degustación_con_vinos.jpg",
+    },
+    {
+        id: 6,
+        productName: "Sushi y vino",
+        price: 2800,
+        quanty: 1,
+        img: "/client/sushi_vinos.jpg",
+    },
+    {
+        id: 7,
+        productName: "Mix vegetales y queso",
+        price: 2100,
+        quanty: 1,
+        img: "/client/media/Mix_vegetales_y_queso.jpg",
+    },
+    {
+        id: 8,
+        productName: "Ensalada proteica con frutos rojos o piña",
+        price: 2000,
+        quanty: 1,
+        img: "/client/media/Ensalada_proteica_con_frutosRojos_o_piña.jpg",
+    },
+ {
+        id: 9,
+        productName: "Mix vegetales y queso (grande)",
+        price: 2400,
+        quanty: 1,
+        img: "/client/media/Mix_vegetales_queso.jpg",
+    },
 ];
