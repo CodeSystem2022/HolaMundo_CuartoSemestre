c929813e931c91a0e06321c91325e06a4dbac45f
Gustavo Ortiz <92409193+Gustaf02@users.noreply.github.com>
2023-10-31 21:01:58 -0300

    Conceptos básicos de promises.js

--- Node/02-Fundamentos_node/async/promises.js
+++ Node/02-Fundamentos_node/async/promises.js
@@ -1,3 +1,19 @@
+/*
+ Conceptos básicos: Un método o proceso síncrono se le denomina cuando un proceso se ejecuta después de otro bajo una secuencia o sincronización, por lo que 
+ un proceso asíncrono, no se encuentra bajo ninguna secuenciay se ejecuta de manera separada la resto de los procesos, 
+ lo cual quiere decir que no espera ningún otro proceso.
+
+Generalmente, los proceso asíncronos se encargan de realizar operaciones de entrada/salida, peticiones a servidores o lectura y escritura 
+de archivos en disco.
+
+La mejor manera de manejar los procesos asíncronos es mediante las promesas, debido a que el método de callbacks presenta ciertas desventajas, 
+los llamados CallBackHell o Pyramid of Doom .
+
+Una promesa en Nodejs, se puede manipular mediante la palabra reservada promise y se encuentra conformada principalmente por retornar 
+dos valores de respuesta: resolve() que devuelve el valor cuando es exitosa la operación realizada de la tarea asíncrona y reject() 
+cuando la operación arroja algún error.
+*/
+
 function hola(nombre) {
     return new Promise(function (resolve, reject) {
         setTimeout(function () {
